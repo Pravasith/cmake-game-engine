d29b6d9454d813dc4372b0f765fbc4d44faf870e
__prav__ <pravasith@gmail.com>
2026-06-23 12:32:21 -0400

    fin: fin fin

--- GameApp/tools/build.sh
+++ GameApp/tools/build.sh
@@ -7,3 +7,7 @@ cmake --install ./build/linux --prefix ../install/linux
 cmake --preset GameAppWindows -S .
 cmake --build ./build/windows
 cmake --install ./build/windows
+
+# mkdir -p ../install/windows/bin
+cp /usr/lib/gcc/x86_64-w64-mingw32/13-win32/lib*.dll \
+   ../install/windows/bin
